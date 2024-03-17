39ff3005de51e4b54cfb1d3a98cc688771695a7c
mohammad <162151729+Japharzadeh@users.noreply.github.com>
2024-03-17 06:31:40 -0700

    Jtor.py

--- Jator.py
+++ Jator.py
@@ -1,9 +1,22 @@
-#this script is for ubnutu systems
+#this script is for unix systems
+#A program written by Mohammad Jafarzadeh with Python language in 2024 
+#which changes the user's IP at any given time.
+#This app is supported by the Tor project
+#The next updates will also be applied to other systems
+
+
+
+
+
+
+
+
 import os
 import time
 import platform
-run = os.system
 import webbrowser
+
+run = os.system
 print(r'''
 /------------\           ---------
 |___******___|         \/***___***\/
